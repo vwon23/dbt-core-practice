f189e36b8a516439f37b9d2592851f15f5f8e8c5
vwon23 <vwon87@gmail.com>
2024-05-09 11:58:07 -0700

    add customers model

---
+++ models/customers.sql
@@ -0,0 +1,40 @@
+with customers as (
+    select
+        id as customer_id,
+        first_name,
+        last_name
+    from raw.jaffle_shop.customers
+),
+
+orders as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from raw.jaffle_shop.orders
+),
+
+customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+    from orders
+    group by customer_id
+),
+
+final as (
+    select
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        co.first_order_date,
+        co.most_recent_order_date,
+        coalesce(co.number_of_orders, 0) as number_of_orders
+    from customers c
+        left join customer_orders co on c.customer_id = co.customer_id
+)
+
+select * from final
