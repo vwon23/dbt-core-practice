3a20bb4b3f1642cb76fd028db8c31a3f521dc531
vwon23 <vwon87@gmail.com>
2024-05-11 23:58:06 -0700

    refactor dim_customer to use fct_orders

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,11 +3,7 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
-),
-
-payments as (
-    select * from {{ ref('stg_payments')}}
+    select * from {{ ref('fct_orders')}}
 ),
 
 customer_orders as (
@@ -15,25 +11,23 @@ customer_orders as (
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(o.order_id) as number_of_orders,
+        count(order_id) as number_of_orders,
         sum(amount) as lifetime_value
-    from orders o
-        left join payments p on o.order_id = p.order_id
-
-    group by customer_id
+    from orders
+    group by 1
 ),
 
 final as (
     select
-        c.customer_id,
-        c.first_name,
-        c.last_name,
-        co.first_order_date,
-        co.most_recent_order_date,
-        coalesce(co.number_of_orders, 0) as number_of_orders,
-        co.lifetime_value
-    from customers c
-        left join customer_orders co on c.customer_id = co.customer_id
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+    from customers
+    left join customer_orders using (customer_id)
 )
 
 select * from final
