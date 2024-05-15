82865188a62f543607f8d655461c05353d65968e
vwon23 <vwon87@gmail.com>
2024-05-14 18:36:19 -0700

    advanced jinja & macros

--- macros/test_target_var.sql
+++
@@ -1,5 +0,0 @@
-{% macro test_target_var() %}
-    {{ log(target.name) }}
-    {{ log(target.role) }}
-    {{ log(target.schema) }}
-{% endmacro %}
---
+++ models/union_tables_by_orders_prefix.sql
@@ -0,0 +1,7 @@
+{{
+    union_tables_by_prefix(
+        database='raw',
+        schema='JAFFLE_SHOP',
+        prefix=''
+    )
+}}
